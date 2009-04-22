2e6e7eb763f05a2d8ae8c4730d9b6f80b8b47566
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2009-04-22 08:16:39 +0000

    ajout de tests unitaires pour le parsing des hosttemplates
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@96 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- unittests/test_conf_parse.py
+++ unittests/test_conf_parse.py
@@ -124,4 +124,106 @@ class ParseHost(unittest.TestCase):
                 "The \"test\" tag is not properly parsed"
 
 
+class ParseHostTemplate(unittest.TestCase):
+
+    def setUp(self):
+        """Call before every test case."""
+        conf.confDir = "../src/conf.d"
+        conf.dataDir = "../src"
+        self.testdatadir = "testdata"
+        ## Prepare temporary directory
+        self.tmpdir = tempfile.mkdtemp(dir="/dev/shm")
+        shutil.copytree(os.path.join(conf.confDir, "general"),
+                        os.path.join(self.tmpdir, "general"))
+        #shutil.copytree(os.path.join(conf.confDir, "hosttemplates"),
+        #                os.path.join(self.tmpdir, "hosttemplates"))
+        os.mkdir(os.path.join(self.tmpdir, "hosttemplates"))
+        os.mkdir(os.path.join(self.tmpdir, "hosts"))
+        conf.confDir = self.tmpdir
+        conf.loadConf()
+        conf.hosttemplatefactory.path = [os.path.join(self.tmpdir, "hosttemplates"),]
+        #shutil.copy(os.path.join(self.testdatadir, "host.xml"), os.path.join(self.tmpdir, "hosts"))
+        #self.host = open(os.path.join(self.tmpdir, "hosts", "host.xml"), "w")
+        self.defaultht = open(os.path.join(self.tmpdir, "hosttemplates", "default.xml"), "w")
+        self.defaultht.write('<?xml version="1.0"?>\n<template name="default"></template>')
+        self.defaultht.close()
+        self.ht = open(os.path.join(self.tmpdir, "hosttemplates", "test.xml"), "w")
+
+    def tearDown(self):
+        """Call after every test case."""
+        shutil.rmtree(self.tmpdir)
+
+
+    def test_template(self):
+        """Test the parsing of a basic template declaration"""
+        self.ht.write("""<?xml version="1.0"?>\n<template name="test"></template>""")
+        self.ht.close()
+        conf.hosttemplatefactory.load_templates()
+        assert "test" in conf.hosttemplatefactory.templates, \
+                "template is not properly parsed"
+
+    def test_attribute(self):
+        self.ht.write("""<?xml version="1.0"?>
+                <template name="test">
+                    <attribute name="testattr">testattrvalue</attribute>
+                </template>""")
+        self.ht.close()
+        conf.hosttemplatefactory.load_templates()
+        assert "testattr" in conf.hosttemplatefactory.templates["test"]["attributes"] and \
+               conf.hosttemplatefactory.templates["test"]["attributes"]["testattr"] == "testattrvalue", \
+                "The \"attribute\" tag is not properly parsed"
+
+    def test_test(self):
+        self.ht.write("""<?xml version="1.0"?>
+                <template name="test">
+                    <test name="TestTest"/>
+                </template>""")
+        self.ht.close()
+        conf.hosttemplatefactory.load_templates()
+        assert {"name": "TestTest"} in conf.hosttemplatefactory.templates["test"]["tests"], \
+                "The \"test\" tag is not properly parsed"
+
+    def test_test_args(self):
+        self.ht.write("""<?xml version="1.0"?>
+                <template name="test">
+                    <test name="TestTest">
+                        <arg name="TestArg1">TestValue1</arg>
+                        <arg name="TestArg2">TestValue2</arg>
+                    </test>
+                </template>""")
+        self.ht.close()
+        conf.hosttemplatefactory.load_templates()
+        assert len(conf.hosttemplatefactory.templates["test"]["tests"]) == 1 and \
+               conf.hosttemplatefactory.templates["test"]["tests"][0]["name"] == "TestTest" and \
+               "TestArg1" in conf.hosttemplatefactory.templates["test"]["tests"][0]["args"] and \
+               "TestArg2" in conf.hosttemplatefactory.templates["test"]["tests"][0]["args"] and \
+               conf.hosttemplatefactory.templates["test"]["tests"][0]["args"]["TestArg1"] == "TestValue1" and \
+               conf.hosttemplatefactory.templates["test"]["tests"][0]["args"]["TestArg2"] == "TestValue2", \
+                "The \"test\" tag with arguments is not properly parsed"
+
+    def test_group(self):
+        self.ht.write("""<?xml version="1.0"?>
+                <template name="test">
+                    <group name="Test group"/>
+                </template>""")
+        self.ht.close()
+        conf.hosttemplatefactory.load_templates()
+        assert "Test group" in conf.hosttemplatefactory.templates["test"]["groups"], \
+                "The \"group\" tag is not properly parsed"
+
+    def test_parent(self):
+        self.ht.write("""<?xml version="1.0"?>
+            <templates>
+                <template name="test1">
+                </template>
+                <template name="test2">
+                    <parent name="test1"/>
+                </template>
+            </templates>""")
+        self.ht.close()
+        conf.hosttemplatefactory.load_templates()
+        assert "test1" in conf.hosttemplatefactory.templates["test2"]["parent"], \
+                "The \"parent\" tag is not properly parsed"
+
+
 # vim:set expandtab tabstop=4 shiftwidth=4:
