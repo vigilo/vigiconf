396dfca5ac6ecdec1912583d94ae7cd71f697570
Eric SIMORRE <eric.simorre@c-s.fr>
2010-01-11 10:49:47 +0000

    test ventilator: initialisation avec ancienne implementation pickle
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1609 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- tests/test_ventilator.py
+++ tests/test_ventilator.py
@@ -5,27 +5,38 @@ ventilator test
 """
 
 
-import unittest
+import os, unittest, shutil
 
 import vigilo.vigiconf.conf as conf
 
 from confutil import reload_conf, setup_tmpdir
 
+from vigilo.vigiconf.lib.ventilator import appendHost, getServerToUse, findAServerForEachHost
+from vigilo.vigiconf.lib.confclasses.host import Host
+from vigilo.vigiconf import generator
 
 class VentilatorTest(unittest.TestCase):
-    """Test Sample"""
-
+    """Test Ventilator"""
+      
     def setUp(self):
         """Call before every test case."""
         # Prepare temporary directory
-        #self.tmpdir = setup_tmpdir()
+        self.tmpdir = setup_tmpdir()
+        os.mkdir(os.path.join(self.tmpdir, "db"))
+        self.basedir = os.path.join(self.tmpdir, "deploy")
+        conf.hosttemplatefactory.load_templates()
+        reload_conf()
     
     def tearDown(self):
         """Call after every test case."""
         #shutil.rmtree(self.tmpdir)
     
-    def test_sample(self):
-        pass
+    def test_add_host(self):
+        #host = Host(conf.hostsConf, "testserver1", "192.168.1.1", "Servers")
+        appendHost('supserver.example.com', 'testserver1')
+        s = getServerToUse(['supserver.example.com',], 'testserver1')
+        self.assertEquals(s, 'supserver.example.com')
+        
 
 if __name__ == '__main__':
     unittest.main()
