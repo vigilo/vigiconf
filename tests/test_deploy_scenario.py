de17528412aae8849960389ef33bff24bc5b0914
Eric SIMORRE <eric.simorre@c-s.fr>
2010-03-10 17:19:47 +0000

    add test scenarion deploiement
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2320 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ tests/test_deploy_scenario.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+Test that the dispatchator works properly
+"""
+
+import sys, os, unittest, tempfile, shutil, glob, re
+
+from vigilo.common.conf import settings
+
+import vigilo.vigiconf.conf as conf
+import vigilo.vigiconf.dispatchator as dispatchator
+from vigilo.vigiconf.lib.confclasses.host import Host
+from vigilo.vigiconf.lib import dispatchmodes
+
+from confutil import reload_conf, setup_tmpdir
+from confutil import setup_deploy_dir, teardown_deploy_dir
+
+
+class DeployScenario(unittest.TestCase):
+
+    def setUp(self):
+        """Call before every test case."""
+        # Prepare necessary directories
+        # TODO commenter les divers repertoires
+        setup_deploy_dir()
+        # Deploy on the localhost only -> switch to Community Edition
+        
+        delattr(conf, "appsGroupsByServer")
+        self.host = Host(conf.hostsConf, "testserver1", "192.168.1.1", "Servers")
+        test_list = conf.testfactory.get_test("UpTime", self.host.classes)
+        self.host.add_tests(test_list)
+        self.dispatchator = dispatchmodes.getinstance()
+        # Disable qualification, validation, stop and start scripts
+        for app in self.dispatchator.getApplications():
+            app.setQualificationMethod("")
+            app.setValidationMethod("")
+            app.setStopMethod("")
+            app.setStartMethod("")
+        # Don't check the installed revisions
+        self.dispatchator.setModeForce(True)
+
+    def tearDown(self):
+        """Call after every test case."""
+        teardown_deploy_dir()
+
+    def test_deploy(self):
+        """Teste un scenario generation, deploiement, saveConfig"""
+        
+        self.dispatchator.generateConfig()
+        
+        self.dispatchator.deploy()
+        
+        self.dispatchator.saveToConfig()
+
+
+
+# vim:set expandtab tabstop=4 shiftwidth=4:
