6904116222c7ff901d9f03046e92665af609b2b2
Eric SIMORRE <eric.simorre@c-s.fr>
2010-04-02 09:52:26 +0000

    WIP: map loader
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2584 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/vigiconf/loaders/map.py
+++ src/vigilo/vigiconf/loaders/map.py
@@ -30,102 +30,117 @@ class MapLoader(XMLLoader):
     
     do_validation = True
     
+    maps = {}
+    node_mode = False
+    submap_mode = False
+    groups = None
+    
     def start_element(self, tag):
-        """ 
+        """ start element event handler
         """
-        pass
+        if   tag == "map": self.start_map()
+        elif tag == "nodes": self.node_mode = True
+        elif tag == "host": self.start_host()
+        elif tag == "service": pass
+        elif tag == "label": self.start_label()
+        elif tag == "position": pass
+        elif tag == "icon": pass
+        elif tag == "stateicon": pass
+        elif tag == "submaps": self.submap_mode = True
+        elif tag == "title": self.title = self.get_utext()
+        elif tag == "bg_position": self.bg_position = self.get_utext()
+        elif tag == "bg_repeat": self.bg_repeat = self.get_utext()
+        elif tag == "bg_color": self.bg_color = self.get_utext()
+        elif tag == "bg_image": self.bg_image = self.get_utext()
+        elif tag == "group": self.start_group()
+        elif tag == "maps": pass
+        elif tag == "links": pass
+        elif tag == "servicelink": pass
+        elif tag == "segment": pass
+        elif tag == "reference": pass
+        elif tag == "from_node": pass
+        elif tag == "to_node": pass
+        else:
+            raise Exception("balise inconnue: <%s>" % tag)
     
     def end_element(self, tag):
+        """ end element event handler
         """
-        """
+        if   tag == "map": self.end_map()
+        elif tag == "nodes": self.node_mode = False
+        elif tag == "host": self.end_host()
+        elif tag == "service": pass
+        elif tag == "label": self.end_label()
+        elif tag == "position": pass
+        elif tag == "icon": pass
+        elif tag == "stateicon": pass
+        elif tag == "submaps": self.submap_mode = False
+        elif tag == "title": pass
+        elif tag == "bg_position": pass
+        elif tag == "bg_repeat": pass
+        elif tag == "bg_color": pass
+        elif tag == "bg_image": pass
+        elif tag == "group": self.end_group()
+        elif tag == "maps": pass
+        elif tag == "links": pass
+        elif tag == "servicelink": pass
+        elif tag == "segment": pass
+        elif tag == "reference": pass
+        elif tag == "from_node": pass
+        elif tag == "to_node": pass
+        else:
+            raise Exception("balise inconnue: <%s>" % tag)
+        
+    def start_map(self):
+        self.mapid = self.get_uattrib("id")
+        self.groups = []
+    
+    def end_map(self):
+        map = Map(
+                mtime=datetime.today(),
+                title=self.title,
+                background_color=self.bg_color,
+                background_image=self.bg_image,
+                background_position=self.bg_position,
+                background_repeat=self.bg_repeat
+                )
+        if len(self.groups) > 0:
+            map.groups = list(self.groups)
+        if self.mapid:
+            self.maps[self.mapid] = map
+        self.mapid = None
+        
+    
+    def start_host(self):
+        if not self.node_mode: return
+        
+        name = self.get_attrib("name")
+        id = self.get_attrib("id")
+        host = Host.by_host_name(name)
+    
+    def end_host(self):
+        if not self.node_mode: return
+        host, name, id = (None,) * 3
+     
+    def start_group(self):
+        groupname = self.get_utext()
+        group = MapGroup.by_group_name(groupname)
+        if not group:
+            group = MapGroup(name=groupname)
+        self.groups.append(group)
+    
+    def end_group(self):
         pass
     
+    def start_label(self):
+        pass
     
-    def load(self, path):
-        """ Loads maps from a xml file.
-        
-            @param filepath: an XML file
-            @type  filepath: C{str}
-        """
-        
-        maps = {}
-        node_mode = False
-        submap_mode = False
-        
-        try:
-            for event, elem in self.get_xml_parser(path):
-                if event == "start":
-                    if elem.tag == "map":
-                        mapid = self.get_uattrib("id", elem)
-                        groups = []
-                    elif elem.tag == "nodes":
-                        node_mode = True
-                    elif elem.tag == "host":
-                        if not node_mode: continue
-                        name = elem.attrib["name"].strip()
-                        id = elem.attrib["id"].strip()
-                        host = Host.by_host_name(name)
-                    elif elem.tag == "service":
-                        if not node_mode: continue
-                        pass
-                    elif elem.tag == "label":
-                        pass
-                    elif elem.tag == "position":
-                        pass
-                    elif elem.tag == "icon":
-                        pass
-                    elif elem.tag == "stateicon":
-                        pass
-                    elif elem.tag == "submaps":
-                        submap_mode = True
-                    elif elem.tag == "title":
-                        title = self.get_utext(elem)
-                    elif elem.tag == "bg_position":
-                        bg_position = self.get_utext(elem)
-                    elif elem.tag == "bg_repeat":
-                        bg_repeat = self.get_utext(elem)
-                    elif elem.tag == "bg_color":
-                        bg_color = self.get_utext(elem)
-                    elif elem.tag == "bg_image":
-                        bg_image = self.get_utext(elem)
-                    elif elem.tag == "group":
-                        groupname = self.get_utext(elem)
-                        group = MapGroup.by_group_name(groupname)
-                        if not group:
-                            group = MapGroup(name=groupname)
-                        groups.append(group)
-                        
-                else:
-                    if elem.tag == "map":
-                        map = Map(
-                                mtime=datetime.today(),
-                                title=title,
-                                background_color=bg_color,
-                                background_image=bg_image,
-                                background_position=bg_position,
-                                background_repeat=bg_repeat
-                                )
-                        if len(groups) > 0:
-                            map.groups = list(groups)
-                        if mapid:
-                            maps[mapid] = map
-                        mapid = None
-                    elif elem.tag == "nodes":
-                        node_mode = False
-                    elif elem.tag == "host":
-                        if not node_mode: continue
-                        pass
-                    elif elem.tag == "service":
-                        if not node_mode: continue
-                        pass
-                    elif elem.tag == "host":
-                        if not node_mode: continue
-                        host, name, id = (None,) * 3
-                    elif elem.tag == "submaps":
-                        submap_mode = False
-                            
-                        
-            DBSession.flush()
-        except:
-            #DBSession.rollback()
-            raise
+    def end_label(self):
+        pass
+    
+    def start_(self):
+        pass
+    
+    def end_(self):
+        pass
+   
