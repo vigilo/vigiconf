c0201c3d6d9e99a4872bb82269c979dde8b937de
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-05-19 10:17:15 +0000

    Dépendances TG -> Genshi
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@3130 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(name='vigilo-vigiconf',
             "vigilo-common",
             "psycopg2",
             "vigilo-models",
-            "vigilo-turbogears"
+            "Genshi",
             ],
         extras_require={
             'tests': tests_require,
