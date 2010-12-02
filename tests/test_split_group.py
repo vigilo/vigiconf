46192cbbba988a49d09a1b9eec1c1a7d40574d24
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-12-02 14:43:56 +0000

    Ajout d'un test sur l'éclatement de la définition d'une hiérarchie de groupes en plusieurs fichiers (#336). Le test peut aussi servir de test de non-régression pour vérifier qu'on ne crée pas 2 fois le même hôte racine.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@5326 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ tests/test_split_group.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from confutil import setup_db, teardown_db, setup_path
+from vigilo.vigiconf.loaders.group import GroupLoader
+from vigilo.models.session import DBSession
+from vigilo.models.tables import SupItemGroup
+
+class TestSplitGroup(unittest.TestCase):
+    def setUp(self):
+        super(TestSplitGroup, self).setUp()
+        setup_path()
+        setup_db()
+
+    def tearDown(self):
+        teardown_db()
+        super(TestSplitGroup, self).tearDown()
+
+    def test_split_group(self):
+        """Test l'éclatement de la définition des groupes (#336)"""
+        # Chargement des groupes.
+        grouploader = GroupLoader()
+        grouploader.load()
+        DBSession.flush()
+
+        # Vérification des groupes créés.
+        split_group = DBSession.query(
+                SupItemGroup
+            ).filter(SupItemGroup.name == u'split_group'
+            ).all()
+
+        children = split_group.children
+        self.assertEquals(2, len(children), "2 groupes fils attendus")
+        names = [c.name for c in children]
+        names.sort()
+        self.assertEquals([u'bar', u'foo'], names)
