9f6a1ac0e6cde5266ae18749e837cc564c4a9588
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-02-07 11:01:28 +0000

    Oubli dans la correction du #116
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@5999 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/vigiconf/applications/nagios/start.sh
+++ src/vigilo/vigiconf/applications/nagios/start.sh
@@ -5,6 +5,7 @@ confdir="$1"
 conffiles="`ls $confdir/nagios/*.cfg 2>/dev/null`"
 if [ -z "$conffiles" ]; then
     echo "No Nagios configuration file, not starting Nagios"
+    exit 0
 fi
 sudo /etc/init.d/%(initname)s start
 
