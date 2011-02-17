75cf0b7eb5b49004f1ae14c922919de40beab746
Vincent QUEMENER <vincent.quemener@c-s.fr>
2011-02-17 13:31:29 +0000

    Ajout d'un test permettant de déterminer si un serveur apache répond pour une URL donnée.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6088 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/vigiconf/tests/all/HTTP_url.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# vim:set expandtab tabstop=4 shiftwidth=4:
+
+class HTTP_url(Test):
+    """Check if the HTTP service is up for a given url"""
+
+    def add_test(self, host, service, port=80, url="/"):
+        """Arguments:
+            host:    the Host object to add the test to
+            service: the name of the service to be used in Nagios
+            port:    the TCP port to test
+            url:     the URL to check
+        """
+
+        # remove http://
+        if url.startswith("http://"):
+            url = url[7:]
+
+        host.add_external_sup_service(service, "check_http_url!%s!%s" %
+            (port, url), weight=self.weight, directives=self.directives)
+
+        host.add_perfdata_handler(service, 'HTTP-time', 'response time', 'time')
+        host.add_graph("HTTP response time", [ 'HTTP-time' ], 'lines', 's')
+
