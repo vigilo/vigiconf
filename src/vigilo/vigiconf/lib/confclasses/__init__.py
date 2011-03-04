559a659739c29782daa830f338f3c672de93c09f
Francois POIROTTE <francois.poirotte@c-s.fr>
2011-03-04 13:22:03 +0000

    Déplacement du parser de chemins (parse_path) de VigiConf vers vigilo-common car la fonction peut être utile pour d'autres modules que VigiConf. Pour assurer la rétro-compatibilité, le module vigilo.vigiconf.lib.confclasses importe désormais la fonction depuis vigilo.common et l'expose à son tour. Ajout d'une méthode `by_path()` sur les groupes qui permet de récupérer un groupe en fonction de son chemin (demande rencontrée chez un client). Cette méthode doit être utilisée avec parcimonie car elle est coûteuse en terme de requêtes SQL.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6272 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/vigiconf/lib/confclasses/__init__.py
+++ src/vigilo/vigiconf/lib/confclasses/__init__.py
@@ -3,9 +3,12 @@
 This module contains the classes used in the configuration system.
 """
 
+from vigilo.common import parse_path
+
 __all__ = [
     'get_text',
     'get_attrib',
+    'parse_path',
 ]
 
 def get_text(elem):
@@ -42,65 +45,3 @@ def get_attrib(elem, attr):
         if not attrib or not attrib.strip():
             return None
         return attrib
-
-def parse_path(path):
-    """
-    Analyse le contenu d'un chemin d'accès et retourne
-    une liste de ses composantes.
-
-    @param path: Chemin d'accès relatif ou absolu.
-    @type path: C{basestr}
-    @return: Ensemble des composantes du chemin d'accès
-        ou None si L{path} ne représente pas un chemin
-        d'accès valide.
-    @rtype: C{set} ou C{None}
-    """
-    parts = []
-
-    # On refuse les chemins d'accès vides.
-    if not path:
-        return None
-
-    absolute = (path[0] == '/')
-    if absolute:
-        path = path[1:]
-    it = iter(path)
-
-    try:
-        portion = ""
-        while True:
-            ch = it.next()
-            # Il s'agit d'une séquence d'échappement.
-            if ch == '\\':
-                ch = it.next()
-                # Les seules séquences reconnus sont "\\" et "\/"
-                # pour échapper le caractère d'échappement et le
-                # séparateur des composantes du chemin respectivement.
-                if ch == '/' or ch == '\\':
-                    portion += ch
-                else:
-                    return None
-            # Il s'agit d'un séparateur de chemins.
-            elif ch == '/':
-                if not portion:
-                    return None
-                parts.append(portion)
-                portion = ""
-            # Il s'agit d'un autre caractère (quelconque).
-            else:
-                portion += ch
-    except StopIteration:
-        if portion:
-            parts.append(portion)
-
-    # Permet de traiter correctement
-    # le cas où le chemin est '/'.
-    if not parts:
-        return None
-
-    # Un chemin relatif ne peut pas
-    # contenir plusieurs composantes.
-    if len(parts) != 1 and (not absolute):
-        return None
-
-    return parts
