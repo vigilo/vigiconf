48503ea78ecaf8d9bba6805daf19be292fd5e49e
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-04-07 16:01:27 +0000

    vigiconf: oubli pour apidoc
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6597 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/vigiconf/applications/netflow/__init__.py
+++ src/vigilo/vigiconf/applications/netflow/__init__.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 # vim: set et ts=4 sw=4:
-from . import generator
 #pylint: disable-msg=C0111
 
 from __future__ import absolute_import
 
 from vigilo.vigiconf.lib.application import Application
-
+from . import generator
 
 
 class Netflow(Application):
