ec4fb2440d0048831abf60c1bc639ccb99ae0b4c
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-04-18 20:55:44 +0000

    Standardisation du format du copyright
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6820 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/vigiconf/lib/generators/file.py
+++ src/vigilo/vigiconf/lib/generators/file.py
@@ -3,7 +3,7 @@
 ################################################################################
 #
 # ConfigMgr Nagios Collector plugin configuration file generator
-# Copyright (C) 2007 CS-SI
+# Copyright (C) 2007-2011 CS-SI
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
