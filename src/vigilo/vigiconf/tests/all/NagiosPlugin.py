2e1b4b8896e9c577f01e75338eeb6ce828229cca
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-05-31 11:31:53 +0000

    Ajout d'un test python générique pour les plugins Nagios
    Reste à faire la métrologie. Refs #709.
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@7237 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/vigiconf/tests/all/NagiosPlugin.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#pylint: disable-msg=C0301,C0111,W0232,R0201,R0903
+# Copyright (C) 2006-2011 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+from vigilo.vigiconf.lib.confclasses.test import Test
+
+
+class NagiosPlugin(Test):
+    """Test générique pour utiliser un plugin Nagios indépendant"""
+
+    oids = [".1.3.6.1.4.1.2.6.191.1.2.1.0"]
+
+    def add_test(self, host, name, command):
+        """
+        @param host: Le nom d'hôte pour le test
+        @param name: Le nom du service dans Nagios (service_description)
+        @param command: Le nom de la commande à utiliser (check_command)
+        """
+        # Service
+        host.add_external_sup_service(name, command,
+                weight=self.weight, directives=self.directives)
+        # TODO: ajouter la métrologie
+
+
+# vim:set expandtab tabstop=4 shiftwidth=4:
