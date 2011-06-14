b4eb7fe63ddc32b15e1ec8ab887634683739e492
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-06-14 12:10:16 +0000

    Nouvelle version publiée
    BuildBot: no-build
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@7290 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ def get_data_files():
 
 
 setup(name='vigilo-vigiconf',
-        version='2.0.1',
+        version='2.0.2',
         author='Vigilo Team',
         author_email='contact@projet-vigilo.org',
         url='http://www.projet-vigilo.org/',
