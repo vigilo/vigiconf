3eb44c83f6d2bb6e46ec7a797f1577d7fded6690
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-10-18 15:37:09 +0200

    Génération de la doc PDF avec LaTeX (plus beau)
    Change-Id: If0440b3de93e88c71f1dd4890ac2ce92655e2901

--- doc/conf.py
+++ doc/conf.py
@@ -6,4 +6,9 @@ pdf_documents = [
         ('util', "vigiconf-util", "VigiConf : Manuel utilisateur", u'Vigilo'),
 ]
 
+latex_documents = [
+  #('%(master_str)s', '%(project_fn)s.tex', u'%(project_doc_texescaped_str)s',
+  ('util', 'util.tex', u'VigiConf : Manuel utilisateur', 'AA100004-2/UTI00003', 'vigilo'),
+]
+
 execfile("../buildenv/doc/conf.py")
