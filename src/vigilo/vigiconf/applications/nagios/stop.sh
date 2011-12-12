44445cabd6874c2eacf4610dae5dad291a0fa745
Francois POIROTTE <francois.poirotte@c-s.fr>
2011-12-12 16:16:17 +0100

    Signal SIGTERM/SIGKILL pour Nagios (#926).
    Dans le script chargé d'envoyer un signal SIGTERM journalier à Nagios,
    on fait attention de ne pas tuer accidentellement le script de
    redémarrage par la même veine (ce qui empêcherait le relancement de
    Nagios à la fin du script).
    
    De la même manière, on fait bien attention à ne pas tuer trop de
    processus (via SIGKILL) dans le script d'arrêt de Nagios de VigiConf.
    
    Change-Id: I00044b3c24cb121997f6bedc9f8de9ee18b8ad3b
    Refs: #926.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/440
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Aurelien BOMPARD <aurelien.bompard@c-s.fr>

--- src/vigilo/vigiconf/applications/nagios/stop.sh
+++ src/vigilo/vigiconf/applications/nagios/stop.sh
@@ -5,5 +5,29 @@ for i in `seq 20`; do
     pgrep %(nagios_bin)s >/dev/null || exit 0
     sleep 1
 done
-sudo -u nagios pkill -9 %(nagios_bin)s
 
+# On tente de déterminer l'emplacement du fichier de PID de Nagios
+# à partir des emplacements les plus probables pour le fichier nagios.cfg.
+nagios_pidfile=
+for d in /etc/nagios3 /etc/nagios; do
+    if test -f "$d/nagios.cfg"; then
+        possible_pidfile=`egrep "^[[:space:]]*lock_file" "$d/nagios.cfg"|cut -d= -f2`
+        # Élimine les espaces éventuels autour du chemin.
+        possible_pidfile=`echo $possible_pidfile`
+        if test -f $possible_pidfile; then
+            nagios_pidfile=$possible_pidfile
+            break
+        fi
+    fi
+done
+
+# Si Nagios n'a pas réussi à terminer correctement
+# avec la manière douce, on tente la manière forte.
+nagios_pid=`head -n 1 $nagios_pidfile 2> /dev/null`
+
+# Pas de (fichier de) PID, on ne fait rien
+# (Nagios s'est probablement arrêté correctement entre temps).
+if test -z "$nagios_pid"; then
+    exit 0
+fi
+sudo -u nagios kill -KILL -- -$nagios_pid 2>/dev/null || :
