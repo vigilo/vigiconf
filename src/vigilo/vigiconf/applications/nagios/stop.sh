c5d833c1e0565021f4f48166021dcb3d532723a1
Francois POIROTTE <francois.poirotte@c-s.fr>
2011-12-21 13:58:48 +0100

    Gestion de Nagios (#930 et #926).
    Meilleur correctif pour le problème du redémarrage journalier de
    Nagios pour éviter des fuites mémoire (#926).
    
    Évite également un problème dans Nagios (#930) à cause duquel le
    processus principal s'arrête correctement alors que les sous-processus
    continuent d'exister.
    
    Dans les 2 cas, les processus tués se limitent à ceux auxquels Nagios
    peut envoyer des signaux et qui ont été lancés par Nagios.
    
    Change-Id: I8328427bd05ca33e567a86eb375ba80ed844679d
    Refs: #926, #930.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/451
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Aurelien BOMPARD <aurelien.bompard@c-s.fr>

--- src/vigilo/vigiconf/applications/nagios/stop.sh
+++ src/vigilo/vigiconf/applications/nagios/stop.sh
@@ -6,28 +6,7 @@ for i in `seq 20`; do
     sleep 1
 done
 
-# On tente de déterminer l'emplacement du fichier de PID de Nagios
-# à partir des emplacements les plus probables pour le fichier nagios.cfg.
-nagios_pidfile=
-for d in /etc/nagios3 /etc/nagios; do
-    if test -f "$d/nagios.cfg"; then
-        possible_pidfile=`egrep "^[[:space:]]*lock_file" "$d/nagios.cfg"|cut -d= -f2`
-        # Élimine les espaces éventuels autour du chemin.
-        possible_pidfile=`echo $possible_pidfile`
-        if test -f $possible_pidfile; then
-            nagios_pidfile=$possible_pidfile
-            break
-        fi
-    fi
-done
-
-# Si Nagios n'a pas réussi à terminer correctement
-# avec la manière douce, on tente la manière forte.
-nagios_pid=`head -n 1 $nagios_pidfile 2> /dev/null`
-
-# Pas de (fichier de) PID, on ne fait rien
-# (Nagios s'est probablement arrêté correctement entre temps).
-if test -z "$nagios_pid"; then
-    exit 0
-fi
-sudo -u nagios kill -KILL -- -$nagios_pid 2>/dev/null || :
+# On compte ici sur le fait que l'utilisateur "nagios"
+# a des droits restreints et ne peut pas killer d'autres
+# processus sans rapport avec Nagios.
+sudo -u nagios pkill -u nagios -KILL %(nagios_bin)s
