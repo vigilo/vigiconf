3d0e9476f79ea306d60f197e17b4a84e38775aa8
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-03-05 13:47:49 +0100

    Correction scripts arrêt/redémarrage de Nagios.
    Corrige la logique des scripts d'arrêt ou de redémarrage de Nagios.
    Le script de redémarrage fonctionnait correctement car il se détectait
    lui-même (contrepartie : il mettait toujours ~20 secondes à s'exécuter).
    
    Désormais, les scripts ne cherchent que les processus dont le nom
    contient "nagios" et appartenant à l'utilisateur "nagios".
    
    Change-Id: Id4e83a4c3697d569fb22687d5b46408e01eea746
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/531
    Reviewed-by: Aurelien BOMPARD <aurelien.bompard@c-s.fr>
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>

--- src/vigilo/vigiconf/applications/nagios/stop.sh
+++ src/vigilo/vigiconf/applications/nagios/stop.sh
@@ -2,11 +2,11 @@
 
 sudo /etc/init.d/%(initname)s stop
 for i in `seq 20`; do
-    pgrep %(nagios_bin)s >/dev/null || exit 0
+    pgrep -u nagios %(nagios_bin)s >/dev/null || exit 0
     sleep 1
 done
 
 # On compte ici sur le fait que l'utilisateur "nagios"
 # a des droits restreints et ne peut pas killer d'autres
 # processus sans rapport avec Nagios.
-sudo -u nagios pkill -u nagios -KILL %(nagios_bin)s
+sudo -u nagios pkill -KILL -u nagios %(nagios_bin)s
