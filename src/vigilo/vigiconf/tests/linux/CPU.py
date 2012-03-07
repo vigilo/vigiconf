9225600450027fefff9d3688c38077771fe474db
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-03-07 13:58:44 +0100

    Ajout données de métrologie pour check_cpu (#708).
    Ajoute les données de performance suivantes au test check_cpu :
    - CPU User
    - CPU Kernel
    - CPU Idle
    - CPU Wait
    
    Pour rappel, ce test est utilisé sur Linux, SunOS & AIX.
    Ces données de métrologie ne sont disponibles que sur Linux et AIX.
    
    Change-Id: I67b544fa1e0372c260e75d71d0abaf7dd7da471e
    Refs: #708.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/540
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    (cherry picked from commit cd9eb7832094182ce2f3ef8074074da13fa03a9c)

--- src/vigilo/vigiconf/tests/linux/CPU.py
+++ src/vigilo/vigiconf/tests/linux/CPU.py
@@ -13,10 +13,32 @@ class CPU(Test):
             warn:   WARNING threshold
             crit:   CRITICAL threshold
         """
+        check_cpu_perf_data = True
+        if "ucd" in host.classes:
+            # ucd donne des informations de métrologie plus complètes.
+            check_cpu_perf_data = False
+
         host.add_external_sup_service( "Sys CPU",
-                    "check_nrpe!check_cpu_args!%s %s" % (warn, crit),
+                    "check_nrpe!check_cpu_args!%s %s %d" %
+                        (warn, crit, check_cpu_perf_data),
                     weight=self.weight, warning_weight=self.warning_weight,
                     directives=self.directives)
+
+        if check_cpu_perf_data:
+            activities = ('User', 'Kernel', 'Idle', 'Wait')
+            for activity in activities:
+                host.add_perfdata_handler(
+                    "Sys CPU",
+                    'CPU %s' % activity,
+                    'CPU %s' % activity,
+                    activity,
+                    'GAUGE')
+            host.add_graph("CPU usage (by type)",
+                           [ "CPU %s" % a for a in activities ],
+                           "stacks", "usage (%)", group="Performance")
+
+        # Dans le cas contraire, ucd ajoutera lui-même le graphe,
+        # avec des indicateurs plus granulaires.
 
 
 # vim:set expandtab tabstop=4 shiftwidth=4:
