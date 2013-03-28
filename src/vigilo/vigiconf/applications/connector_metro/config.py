c589144896d5d8a6e1cb376d130c29bb0205718b
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2013-03-28 17:55:27 +0100

    Label d'affichage des services windows
    Change-Id: Ia9b2165d7622e7bf7d3c5a9e2cc864e47067d24c
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1131
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- src/vigilo/vigiconf/applications/connector_metro/config.py
+++ src/vigilo/vigiconf/applications/connector_metro/config.py
@@ -7,14 +7,23 @@ DEFAULTS = {
     "heartbeat": 600,
     "rra": {
         "basic": [
+            # step=300 secondes => 5 minutes
+            # période=5minutes, 600 valeurs => 3000minutes=50heures (~ 2 jours)
             { "type": "AVERAGE", "xff": 0.5, "step": 1, "rows": 600 },
+            # période=6*5 minutes (30minutes), 700 valeurs => 21000minutes=350heures (~ 14.5 jours)
             { "type": "AVERAGE", "xff": 0.5, "step": 6, "rows": 700 },
+            # période=24*5 minutes (2heures), 775 valeurs => 1550heures=64jours (~ 2 mois)
             { "type": "AVERAGE", "xff": 0.5, "step": 24, "rows": 775 },
+            # période=288*5 minutes (24heures), 775 valeurs => 775jours (~ 2 ans)
             { "type": "AVERAGE", "xff": 0.5, "step": 288, "rows": 732},
         ],
         "netflow": [
+            # step=300 secondes => 5 minutes
+            # période=5minutes, 4320 valeurs => 21600minutes=360heures (15 jours)
             { "type": "AVERAGE", "xff": 0.5, "step": 1, "rows": 4320 },
+            # période=12*5minutes (1heure), 744 valeurs => 744heures=31jours (~1 mois)
             { "type": "AVERAGE", "xff": 0.5, "step": 12, "rows": 744 },
+            # période=288*5minutes(1jour), 366 valeurs => 366jours (1 an)
             { "type": "AVERAGE", "xff": 0.5, "step": 288, "rows": 366},
         ],
         # Cette définition de RRAs est utile
