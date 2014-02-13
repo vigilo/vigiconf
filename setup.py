2678a0de5cb6e3ead917222a849fcd00851c01b9
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2014-02-13 15:38:56 +0100

    Bump des versions (livraison r3.4)
    Change-Id: Ifa118ba2e6896f30e7080971f6d504b66e0084d6
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1370
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>
    Tested-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ def get_data_files():
 
 
 setup(name='vigilo-vigiconf',
-        version='3.3',
+        version='3.4',
         author='Vigilo Team',
         author_email='contact@projet-vigilo.org',
         url='http://www.projet-vigilo.org/',
