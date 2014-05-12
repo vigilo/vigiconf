0fed4f4c4a16d73da3dcce9609e0c8bc5f5896c1
Francois POIROTTE <francois.poirotte@c-s.fr>
2014-05-12 14:30:48 +0200

    Bump des versions
    Change-Id: I16667f5212c272c4185f14412f1fa67d6831f04c
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1412
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ def get_data_files():
 
 
 setup(name='vigilo-vigiconf',
-        version='3.4',
+        version='3.5',
         author='Vigilo Team',
         author_email='contact@projet-vigilo.org',
         url='http://www.projet-vigilo.org/',
