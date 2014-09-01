505ff3913307ab25009c8e6c1c7959ac405a6e52
Francois POIROTTE <francois.poirotte@c-s.fr>
2014-09-01 15:35:21 +0200

    [#1264] Personnalisation des seuils de l'UpTime.
    Permet de personnaliser les seuils utilisés dans le test d'UpTime pour
    la génération d'alertes de niveau WARNING/CRITICAL.
    
    Change-Id: If3cf5bfd25b89b104d027eaaaf2f02285a47a5f7
    Refs: #1264.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1467
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

--- src/vigilo/vigiconf/tests/all/UpTime.py
+++ src/vigilo/vigiconf/tests/all/UpTime.py
@@ -4,6 +4,10 @@
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 from vigilo.vigiconf.lib.confclasses.test import Test
+from vigilo.vigiconf.lib import VigiConfError
+
+from vigilo.common.gettext import translate
+_ = translate(__name__)
 
 
 
@@ -12,8 +16,30 @@ class UpTime(Test):
 
     oids = [".1.3.6.1.2.1.1.3.0"]
 
-    def add_test(self):
-        self.add_collector_service("UpTime", "sysUpTime", [],
+    def add_test(self, warn=900, crit=400):
+        """
+        Teste la disponibilité de l'équipement (durée depuis son dernier
+        redémarrage, ou en réalité, depuis le dernier redémarrage de
+        son agent SNMP).
+
+        @param warn: Durée en secondes pendant laquelle une alerte
+            de niveau WARNING est émise après le redémarrage
+            de l'équipement ou de l'agent SNMP.
+        @type  warn: C{int}
+        @param crit: Durée en secondes pendant laquelle une alerte
+            de niveau CRITICAL est émise après le redémarrage
+            de l'équipement ou de l'agent SNMP.
+            Cette valeur doit être strictement inférieure à celle
+            du paramètre L{warn}.
+        @type  crit: C{int}
+        """
+        warn = self.as_int(warn)
+        crit = self.as_int(crit)
+
+        if warn < 0 or crit < 0 or warn < crit:
+            raise VigiConfError(_('Invalid thresholds. Expected 0 < crit < warn'))
+
+        self.add_collector_service("UpTime", "sysUpTime", [crit, warn],
                             ["GET/.1.3.6.1.2.1.1.3.0"])
         self.add_collector_metro("sysUpTime", "m_sysUpTime", [],
                             ["GET/.1.3.6.1.2.1.1.3.0"], 'GAUGE', label="UpTime")
