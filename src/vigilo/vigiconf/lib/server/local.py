ce595c4db406f891febafbf86c3e90af77cc1ecd
Francois POIROTTE <francois.poirotte@c-s.fr>
2016-04-12 14:49:07 +0200

    [#1525] Correction adresse de la FSF
    Change-Id: I1d86a3e6450422f3775eb3c8e94b19ebd1e03510
    Refs: #1525
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/2075
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- src/vigilo/vigiconf/lib/server/local.py
+++ src/vigilo/vigiconf/lib/server/local.py
@@ -1,19 +1,6 @@
 # -*- coding: utf-8 -*-
-################################################################################
-#
 # Copyright (C) 2007-2016 CS-SI
-#
-# This program is free software; you can redistribute it and/or modify
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-################################################################################
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """
 Ce module contient les implémentations des classes L{Server<base.Server>} et
