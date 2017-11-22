e81a1ead111175be64ee50a39d7dc28f031871fd
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-11-22 13:05:39 +0100

    [#1726] Ajout de tests pour LDAP/LDAPS
    Change-Id: Ibb72b20cb153035136c5cfe5190334947a39706b
    Refs: #1726

---
+++ src/vigilo/vigiconf/tests/all/LDAP.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#pylint: disable-msg=C0301,C0111,W0232,R0201,R0903,W0221
+# Copyright (C) 2011-2016 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+from vigilo.vigiconf.lib.confclasses.test import Test
+
+
+
+class LDAP(Test):
+    """Vérifie la disponibilité d'un serveur LDAP"""
+
+    def add_test(self, base):
+        """
+        @param base: Base de recherche dans l'annuaire.
+        @type  base: C{str}
+        """
+        self.add_external_sup_service("LDAP", "check_ldap!%s" % base)
+        self.add_perfdata_handler("LDAP", 'LDAP-time', 'response time', 'time')
+        self.add_graph("LDAP response time", [ 'LDAP-time' ], 'lines', 's')
+
+
+# vim:set expandtab tabstop=4 shiftwidth=4:
---
+++ src/vigilo/vigiconf/tests/all/LDAPS.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#pylint: disable-msg=C0301,C0111,W0232,R0201,R0903,W0221
+# Copyright (C) 2011-2016 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+from vigilo.vigiconf.lib.confclasses.test import Test
+
+
+
+class LDAPS(Test):
+    """Vérifie la disponibilité d'un serveur LDAP en utilisant SSL/TLS"""
+
+    def add_test(self, base, starttls=False):
+        """
+        @param  base:       Base de recherche dans l'annuaire.
+        @type   base:       C{str}
+        @param  starttls:   Indique si le mécanisme STARTTLS est utilisé ou non.
+        @type   starttls:   C{bool}
+        """
+        starttls = self.as_bool(starttls)
+        if starttls:
+            service = "LDAPS-STARTTLS"
+            check = "check_ldaps"
+        else:
+            service = "LDAPS"
+            check = "check_ldap_ssl"
+
+        self.add_external_sup_service(service, "%s!%s" % (check, base))
+        self.add_perfdata_handler(service, '%s-time' % service, 'response time', 'time')
+        self.add_graph("%s response time" % service, [ '%s-time' % service ], 'lines', 's')
+
+
+# vim:set expandtab tabstop=4 shiftwidth=4:
