0fe47fed8a1766dafca681598d077c8eead962e6
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-12-05 11:12:59 +0100

    [#1730] Ajout d'une sonde pour DNS
    Vérifie le temps de réponse d'un serveur DNS.
    
    Change-Id: I5ccc776d9c06992cc6dbe2dc00d6acd5a7c66561
    Refs: #1730

---
+++ src/vigilo/vigiconf/tests/all/DNS.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#pylint: disable-msg=C0301,C0111,W0232,R0201,R0903,W0221
+# Copyright (C) 2017 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+from vigilo.vigiconf.lib.confclasses.test import Test
+from vigilo.common.gettext import translate
+_ = translate(__name__)
+
+
+class DNS(Test):
+    """Vérifie la disponibilité d'un serveur DNS"""
+
+    def add_test(self):
+        """
+        Teste la disponibilité d'un serveur DNS et lève une
+        alarme dont la gravité dépend du temps de réponse
+        du serveur.
+        """
+
+        # On passe une chaîne vide en tant que requête,
+        # ce qui permet de tester simplement le temps de réponse du serveur,
+        # sans faire de suppositions sur les enregistrements qu'il contient.
+        self.add_external_sup_service("DNS", "check_dig!''")
+        self.add_perfdata_handler("DNS", "DNS-time", "response time", "time")
+        self.add_graph("DNS response time", ["DNS-time"], "lines", "s")
+
+
+# vim:set expandtab tabstop=4 shiftwidth=4:
