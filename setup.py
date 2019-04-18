e3d60a1d4291626cf0f10dc4a8d7ff3f5e7a94d3
Francois POIROTTE <francois.poirotte@c-s.fr>
2019-04-18 14:13:29 +0200

    Préparation alpha2
    Change-Id: I971a946a5e3e1f22a95deb722862e471e5bc5531

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ def get_data_files():
 
 
 setup(name='vigilo-vigiconf',
-        version='5.1.0a1',
+        version='5.1.0a2',
         author='Vigilo Team',
         author_email='contact.vigilo@c-s.fr',
         url='https://www.vigilo-nms.com/',
