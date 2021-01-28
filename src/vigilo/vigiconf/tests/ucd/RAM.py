15fc8b12b156b28c5e22007df310bc1493bcea5e
Francois POIROTTE <francois.poirotte@c-s.fr>
2021-01-28 16:50:10 +0100

    Update the copyrights
    Change-Id: I2666eb73248493b0dc4b2e661faa65beee6f1ae5

--- src/vigilo/vigiconf/tests/ucd/RAM.py
+++ src/vigilo/vigiconf/tests/ucd/RAM.py
@@ -1,6 +1,6 @@
 # vim:set fileencoding=utf-8 expandtab tabstop=4 shiftwidth=4:
 # pylint: disable-msg=C0301,C0111,W0232,R0201,R0903,W0221
-# Copyright (C) 2006-2020 CS GROUP - France
+# Copyright (C) 2006-2021 CS GROUP - France
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 from __future__ import unicode_literals
